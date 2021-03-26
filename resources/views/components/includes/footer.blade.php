edc19656e233e189457824ebac1abbadd4a746e4
nhardbalansag <nhardbalansag@gmail.com>
2021-03-26 10:18:15 +0800

    add footer

--- resources/views/components/includes/footer.blade.php
+++ resources/views/components/includes/footer.blade.php
@@ -1,8 +1,9 @@
   <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+  <footer class="@auth main-footer @endauth   @guest pr-3 pl-3 @endguest">
+    <strong>Copyright &copy; 2014-2020.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0-rc
+      <b>AdminLTE Version</b> 3.1.0-rc
     </div>
   </footer>
+
