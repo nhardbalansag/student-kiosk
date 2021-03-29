559a50eda0fbf14125fc3f71685cbbb01abef57a
nhardbalansag <nhardbalansag@gmail.com>
2021-03-30 03:19:14 +0800

    adding forms

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Main\Query\QueryBuilder;
 use Illuminate\Support\Facades\Validator;
+use DB;
 
 class AdminController extends Controller
 {
@@ -34,6 +35,132 @@ class AdminController extends Controller
             unset($request['_token']);
 
             if(!QueryBuilder::createCurriculum($request)){
+                return redirect()->back()->with('message', 'Data successfully added');
+            }else{
+                return redirect()->back()->with('error', 'Error adding data');
+            }
+        }
+    }
+
+    public function view_add_year_level_curriculum(){
+        return view('components.contents.add-year-level');
+    }
+
+    public function submit_year_level(Request $request){
+
+        $rules = [
+           'year_title' => ['required', 'string', 'max:255'],
+           'description' => ['required', 'string', 'max:255'],
+           'status' => ['required', 'string', 'max:255']
+       ];
+
+       $validator = Validator::make($request->all(),$rules);
+
+       if ($validator->fails()) {
+
+           return redirect()->back()->with('error', 'Error adding data');
+
+       }else{
+
+           unset($request['_token']);
+
+           if(!QueryBuilder::createYearLevel($request)){
+               return redirect()->back()->with('message', 'Data successfully added');
+           }else{
+               return redirect()->back()->with('error', 'Error adding data');
+           }
+       }
+   }
+
+   public function view_add_course(){
+        return view('components.contents.add-course');
+    }
+
+    public function submit_course(Request $request){
+
+        $rules = [
+            'course_title' => ['required', 'string', 'max:255'],
+            'course_description' => ['required', 'string', 'max:255'],
+            'status' => ['required', 'string', 'max:255']
+        ];
+
+        $validator = Validator::make($request->all(),$rules);
+
+        if ($validator->fails()) {
+
+            return redirect()->back()->with('error', 'Error adding data');
+
+        }else{
+
+            unset($request['_token']);
+
+            if(!QueryBuilder::createCourse($request)){
+                return redirect()->back()->with('message', 'Data successfully added');
+            }else{
+                return redirect()->back()->with('error', 'Error adding data');
+            }
+        }
+    }
+
+    public function view_add_semester(){
+        return view('components.contents.add-semester');
+    }
+
+    public function submit_semester(Request $request){
+
+        $rules = [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:255'],
+            'status' => ['required', 'string', 'max:255']
+        ];
+
+        $validator = Validator::make($request->all(),$rules);
+
+        if ($validator->fails()) {
+
+            return redirect()->back()->with('error', 'Error adding data');
+
+        }else{
+
+            unset($request['_token']);
+
+            if(!QueryBuilder::createSemester($request)){
+                return redirect()->back()->with('message', 'Data successfully added');
+            }else{
+                return redirect()->back()->with('error', 'Error adding data');
+            }
+        }
+    }
+
+    public function view_add_subject(){
+
+        $data['subjectList'] = QueryBuilder::getAllData('subjects', 'active');
+        
+        return view('components.contents.add-subject', $data);
+    }
+
+    public function submit_subject(Request $request){
+
+        $rules = [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:255'],
+            'total_units' => ['required', 'numeric'],
+            'lecture_units' => ['required', 'numeric'],
+            'lab_units' => ['required', 'numeric'],
+            'status' => ['required', 'string', 'max:255']
+        ];
+
+        $validator = Validator::make($request->all(),$rules);
+
+        if ($validator->fails()) {
+
+            return redirect()->back()->with('error', 'Error adding data');
+
+        }else{
+
+            unset($request['_token']);
+
+            if(!QueryBuilder::createSubject($request)){
                 return redirect()->back()->with('message', 'Data successfully added');
             }else{
                 return redirect()->back()->with('error', 'Error adding data');
--- app/Main/Query/QueryBuilder.php
+++ app/Main/Query/QueryBuilder.php
@@ -3,7 +3,8 @@
 namespace App\Main\Query;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Main\Model\Curriculum;
+use App\Main\Model\{Curriculum, StudentYear, Course, Semester, Subject};
+use Illuminate\Support\Facades\{Validator, DB};
 
 class QueryBuilder extends Model
 {
@@ -15,5 +16,41 @@ class QueryBuilder extends Model
             'description' => $request['description'],
             'status' => $request['status']
         ]);
+    }
+
+    public static function createYearLevel($request){
+        StudentYear::create([
+            'year_title' => $request['year_title'],
+            'description' => $request['description'],
+            'status' => $request['status']
+        ]);
+    }
+
+    public static function createCourse($request){
+        Course::create([
+            'course_title' => $request['course_title'],
+            'course_description' => $request['course_description'],
+            'status' => $request['status']
+        ]);
+    }
+
+    public static function createSubject($request){
+        Subject::create([
+            'title' => $request['title'],
+            'description' => $request['description'],
+            'total_units' => $request['total_units'],
+            'lecture_units' => $request['lecture_units'],
+            'lab_units' => $request['lab_units'],
+            'subject_id' => $request['subject_id'],
+            'status' => $request['status']
+        ]);
+    }
+
+    public static function getAllData($colection, $filter){
+        $data = DB::table($colection)
+                ->where('status', $filter)
+                ->get();
+
+        return $data;
     }
 }
