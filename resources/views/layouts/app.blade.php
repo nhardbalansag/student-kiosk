0778738f224c936e346bff65a38a6ee56c610fed
nhardbalansag <nhardbalansag@gmail.com>
2021-05-22 06:52:55 +0800

    add logo in tabs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" href="{{ asset('image/University_of_San_Agustin_Logo.png') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
